e5428e3a4f7c59565820509c9ce6027ca947c211
hmahmud01 <hmahmud01@gmail.com>
2019-04-04 12:42:51 +0600

    c-57 : created basic Structure
    All folder structures has been maintained. didn't add screens and navigations, because they may not be required for react app. For development environement, react-router, react-router-dom and redux-saga has been installed.

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {}
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Comingoo Admin Panel</h1>
+                <p>Coming Soon</p>
+            </div>
+        );
+    }
+}
+
+export default App;
